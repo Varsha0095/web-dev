12a52ed9976f8948fb1e76d16bf5297d1c288f6e
Varsha0095 <varsha03prajapati@gmail.com>
2022-09-13 10:19:48 +0530

    adding task-3

---
+++ dom.js
@@ -0,0 +1,30 @@
+
+// //console.dir(document);
+// console.log(document.domain);
+// console.log(document.title);
+// console.log(document.URL);
+// //document.title = 123;
+// console.log(document.doctype);
+// console.log(document.all);
+// console.log(document.head);
+// console.log(document.body);
+
+// //console.log(document.all[10].textContent='hello');
+// document.all[10].textContent = 'Hello';
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.image);
+
+//Getting element by id
+// console.log(document.getElementById('main-header'));
+// console.log(document.getElementById('header-title'));
+let headerTitle = document.getElementById('header-title');
+let header = document.getElementById('main-header');
+console.log(headerTitle);
+ 
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerHTML = '<h4>Heya</h4>';
+header.style.borderBottom = '5px solid #000';
+
+// console.log(headerTitle.innerText);
+
