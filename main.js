6f5e7fdabc25e25e003eb2207ab1fc4c35814b75
Varsha0095 <varsha03prajapati@gmail.com>
2022-09-14 12:17:21 +0530

    editing

--- main.js
+++ main.js
@@ -8,10 +8,12 @@ function addItem(e){
 
 //getting input value
 var newItem = document.getElementById('item').value;
+var descItem = document.getElementById('newit').value;
 //creating new li element
 var li = document.createElement('li');
 li.className = 'list-group-item';
 li.appendChild(document.createTextNode(newItem));
+li.appendChild(document.createTextNode(' '+ descItem));
 //creating delete button element
 var delBtn = document.createElement('button');
 delBtn.className = 'btn btn-danger btn-sm float-right delete';      //adding class to del btn
@@ -19,7 +21,7 @@ delBtn.appendChild(document.createTextNode('X'));                   //adding tex
 li.appendChild(delBtn);                                             //appending del btn to li
 //creating edit button
 var editBtn = document.createElement('button');
-editBtn.className = 'btn btn-danger btn-sm float-right delete';
+editBtn.className = 'btn btn-sm float-right edit'
 editBtn.appendChild(document.createTextNode('EDIT'));
 li.appendChild(editBtn);
 
@@ -35,4 +37,23 @@ function removeItem(e){                                      //creating removeit
             itemList.removeChild(li);           //removing 
         }
     }
+}
+//FILTER ITEMS
+var filter = document.getElementById('filter');
+filter.addEventListener('keyup', filterItems);
+
+function filterItems(e){
+    var text = e.target.value.toLowerCase();        //converting text to lower case
+    var items = itemList.getElementsByTagName('li');  //array of nodes
+    Array.from(items).forEach(function(item) {          //converting from HTML collection to array collection
+        var itemName = item.firstChild.textContent;
+        var desName = item.childNodes[1].textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1 || desName.toLowerCase().indexOf(text) != -1)
+        {
+            item.style.display = 'block';
+        }
+        else{
+            item.style.display = 'none';
+        }
+    });
 }
